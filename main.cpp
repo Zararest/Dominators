f2056df69e73e1356fd7e0539dbb52918527ad8a
Ivan Shumakov <ivan.shumakov@syntacore.com>
2023-08-12 00:44:19 +0300

    Notes

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
   auto Root = Node::Builder{}.createNode();
   ReducibleGraphBuilder<NamedNode> Obj{*Root.get()};
-  for (int i = 0; i < 100; i++) {
+  for (int i = 0; i < 1000; i++) {
     Obj.mutate();
   }
   Obj.printGraph(std::cout);
